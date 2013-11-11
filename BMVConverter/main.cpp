b9dbc4e31776e444e65c9c7b9f18282bd8a546eb
iron-phoenix <kluchikhin@gmail.com>
2013-11-11 23:35:47 +0400

    BMPConverter for storage

---
+++ BMVConverter/main.cpp
@@ -0,0 +1,69 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QtGui/QPixmap>
+#include <QDebug>
+
+QByteArray convertFileToByteArray(QString const &fileinput){
+    QFile in(fileinput);
+    QFile filebmp("/home/phoenix/tmp.bmp");
+
+    if(!in.open(QIODevice::ReadOnly)) return 0;
+    if(!filebmp.open(QIODevice::ReadOnly)) return 0;
+
+    QByteArray ba = in.readAll();
+    QByteArray tmp = filebmp.readAll();
+    unsigned int size = ba.length() + 66;
+    for(size_t i = 0; i != 4; ++i){
+        tmp.data()[2 + i] = (char)size;
+        size >>= 8;
+    }
+    return tmp += ba;
+}
+
+QByteArray convertBMPToByteArray(QString const &fileinput){
+    QFile in(fileinput);
+
+    if(!in.open(QIODevice::ReadOnly)) return 0;
+
+    in.seek(66);
+    QByteArray ba = in.readAll();
+    return ba;
+}
+
+void uploadFiles(QByteArray data, QString inputFileName){
+    if(data.length() <= 200 * 1024 * 1024){
+        uploadFile(data, inputFileName + ".bmp");
+    }
+    else{
+        size_t part_number = 1;
+        while(data.length() > (200 * 1024 * 1024) * part_number){
+            QByteArray tmp = data.mid((200 * 1024 * 1024) * (part_number - 1), 200 * 1024 * 1024 - 1);
+            uploadFile(tmp, inputFileName + ".part" + QString::number(part_number) + ".bmp");
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    QFile savefile("/home/phoenix/1.bmp");
+
+    if(!savefile.open(QIODevice::WriteOnly)) return 0;
+
+    QByteArray ba = convertFileToByteArray("/home/phoenix/Downloads/Filter - Happy Together [pleer.com].mp3");
+
+    savefile.write(ba, ba.length());
+
+
+    savefile.close();
+
+    QFile decodedFile("/home/phoenix/1.mp3");
+
+    if(!decodedFile.open(QIODevice::WriteOnly)) return 0;
+
+    QByteArray baE = convertBMPToByteArray("/home/phoenix/1.bmp");
+    decodedFile.write(baE, baE.length());
+
+    decodedFile.close();
+    return 0;
+}
