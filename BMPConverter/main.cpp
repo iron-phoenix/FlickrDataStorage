82919fef3c0ee907d4a3a532abc16ab0597db9c2
iron-phoenix <kluchikhin@gmail.com>
2013-11-12 00:31:15 +0400

    Refactoring of uploading

--- BMPConverter/main.cpp
+++ BMPConverter/main.cpp
@@ -3,72 +3,76 @@
 #include <QtGui/QPixmap>
 #include <QDebug>
 
-QByteArray convertFileToByteArray(QString const &fileinput){
-    QFile in(fileinput);
+QByteArray convertBytesToBMPBytes(QByteArray const &data){
     QFile filebmp("/home/phoenix/tmp.bmp");
-
-    if(!in.open(QIODevice::ReadOnly)) return 0;
     if(!filebmp.open(QIODevice::ReadOnly)) return 0;
 
-    QByteArray ba = in.readAll();
     QByteArray tmp = filebmp.readAll();
-    unsigned int size = ba.length() + 66;
+    unsigned int size = data.length() + 66;
     for(size_t i = 0; i != 4; ++i){
         tmp.data()[2 + i] = (char)size;
         size >>= 8;
     }
-    return tmp += ba;
+    return tmp += data;
 }
 
-QByteArray convertBMPToByteArray(QString const &fileinput){
-    QFile in(fileinput);
-
-    if(!in.open(QIODevice::ReadOnly)) return 0;
-
-    in.seek(66);
-    QByteArray ba = in.readAll();
-    return ba;
+QByteArray convertBMPBytesToBytes(QByteArray const &bmpdata){
+    return QByteArray(bmpdata.mid(66));
 }
 
-void uploadFiles(QByteArray data, QString inputFileName){
+void uploadBytes(QByteArray, QString){
+
+}
+
+bool uploadFile(QString const &inputFileName){
+    QFile in(inputFileName);
+    if(!in.open(QIODevice::ReadOnly)) return false;
+    QByteArray data = in.readAll();
     if(data.length() <= 200 * 1024 * 1024){
-        uploadFile(data, inputFileName + ".bmp");
+        uploadBytes(convertBytesToBMPBytes(data), inputFileName + ".bmp");
     }
     else{
         size_t part_number = 1;
-        QByteArray header = data.mid(0, 66);
-        data = data.mid(67, -1);
-        while(data.length() > (200 * 1024 * 1024) * part_number){
+        while((unsigned int)data.length() > (200 * 1024 * 1024) * part_number){
             QByteArray tmp = data.mid((200 * 1024 * 1024) * (part_number - 1), 200 * 1024 * 1024);
-            uploadFile(tmp, inputFileName + ".part" + QString::number(part_number) + ".bmp");
+            uploadBytes(convertBytesToBMPBytes(tmp), inputFileName + ".part" + QString::number(part_number) + ".bmp");
             ++part_number;
         }
         QByteArray tmp = data.mid((200 * 1024 * 1024) * (part_number - 1), 200 * 1024 * 1024);
-        uploadFile(tmp, inputFileName + ".part" + QString::number(part_number) + ".bmp");
+        uploadBytes(tmp, inputFileName + ".part" + QString::number(part_number) + ".bmp");
     }
+    return true;
 }
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
     QFile savefile("/home/phoenix/1.bmp");
+    QFile in("/home/phoenix/Downloads/Filter - Happy Together [pleer.com].mp3");
 
+    if(!in.open(QIODevice::ReadOnly)) return 0;
     if(!savefile.open(QIODevice::WriteOnly)) return 0;
 
-    QByteArray ba = convertFileToByteArray("/home/phoenix/Downloads/Filter - Happy Together [pleer.com].mp3");
+    QByteArray inArray = in.readAll();
 
-    savefile.write(ba, ba.length());
+    QByteArray result = convertBytesToBMPBytes(inArray);
+
+    savefile.write(result, result.length());
 
 
+    in.close();
     savefile.close();
 
+    QFile encodedFile("/home/phoenix/1.bmp");
     QFile decodedFile("/home/phoenix/1.mp3");
 
+    if(!encodedFile.open(QIODevice::ReadOnly)) return 0;
     if(!decodedFile.open(QIODevice::WriteOnly)) return 0;
 
-    QByteArray baE = convertBMPToByteArray("/home/phoenix/1.bmp");
-    decodedFile.write(baE, baE.length());
+    QByteArray baE = encodedFile.readAll();
+    result = convertBMPBytesToBytes(baE);
+    decodedFile.write(result, result.length());
 
+    encodedFile.close();
     decodedFile.close();
     return 0;
 }
